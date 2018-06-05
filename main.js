0f8347b0429caeab39159aecdc27fe99599708e5
Marcio Galli <taboca@gmail.com>
2018-06-05 00:04:06 -0300

    Fix what worked

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const {app, BrowserWindow} = require('electron');
+const createVideoRecorder = require('electron-recorder')
+
+console.log('init');
 
 let win;
 
 app.on('ready', () => {
   win = new BrowserWindow();
-  win.loadURL('file://' + __dirname + '/browser.html');
+  win.once('did-finish-load', () => {
+  //  win.show()
+  })
+  win.loadURL('file://' + __dirname + '/recorder.html');
+
 });
--- test.js
+++ test.js
@@ -2,30 +2,29 @@ var Application = require('spectron').Application;
 var electron    = require('electron-prebuilt');
 
 var app = new Application({
-  path: electron, args:['.']
+  path: electron, args:['.'],env: {ELECTRON_ENABLE_LOGGING: true}
 })
 
-app.start().then( 
+app.start().then(
+
+
   function started() {
-    app.client.waitUntilTextExists('#message', 'success', 10000).then(
-      function textShown() {
-        console.log('Success, content loaded!!!');
-        app.stop().then(
-          function stopOk() {
-            console.log('Quit ok!')
-          },
-          function stopFail() {
-            // quit not ok
-          }
-        );
-      }, 
-      function timeOut() { 
-        // If it passes 10 secs..
-        console.log('Make sure your main app provide an HTML page with an element being id=message and its inner HTML equals to "success"');
-      }
-    );
+
+    app.client.getMainProcessLogs().then(function (logs) {
+      logs.forEach(function (log) {
+        console.log('main log: '+ log)
+      })
+    })
+
+    app.client.getRenderProcessLogs().then(function (logs) {
+    	  logs.forEach(function (log) {
+    	    console.log('Render log: '+ log.message + ' - '+ log.source + ' - '+ log.level)
+    	  });
+    	});
+
+
   },
   function not_started() {
     // app.start failed..
-  } 
+  }
 );
